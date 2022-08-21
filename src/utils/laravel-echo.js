2b1d7f36d0e13c166b818b4ae34aebbc07dbd8b3
yangliuancn <yangliuan@foxmail.com>
2022-08-21 14:19:14 +0800

    md websokect

--- src/utils/laravel-echo.js
+++ src/utils/laravel-echo.js
@@ -4,11 +4,10 @@ window.pusher = require('pusher-js');
 
 const laraecho = new Echo({
     broadcaster: 'pusher',
-    key: '20220725',
-    authEndpoint: process.env.VUE_APP_API_BASE_URL + '/broadcasting/auth',
-    wsHost: process.env.VUE_APP_WS_BASE_URL,
-    //wsHost: '127.0.0.1',
-    wsPort: 6001,
+    key: process.env.VUE_APP_LARAECHO_KEY,
+    // authEndpoint: process.env.VUE_APP_API_BASE_URL + '/broadcasting/auth',
+    wsHost: process.env.VUE_APP_LARAECHO_WS_BASE_URL,
+    wsPort: process.env.VUE_APP_LARAECHO_WS_PORT,
     forceTLS: false,
     enabledTransports: ['ws'],
 })
