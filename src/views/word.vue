a54c990c9d24bf97935fd5714cb43dbe10975be7
yangliuan <yangliuancn@foxmail.com>
2022-08-23 22:19:36 +0800

    md

--- src/views/word.vue
+++ src/views/word.vue
@@ -11,12 +11,26 @@
     <el-upload
       class="upload-demo"
       drag
-      action=""
+      accept=".doc,.docx,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document,application/wps-office.docx"
+      :action="upload_url"
+      :on-success="uploadSuccess"
+      name="word"
       multiple>
       <i class="el-icon-upload"></i>
       <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-      <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
+      <div class="el-upload__tip" slot="tip">word转htmldemo，只能上传word文件</div>
     </el-upload>
+    <el-dialog
+      title="提示"
+      :visible.sync="dialogVisible"
+      width="30%"
+      :before-close="handleClose">
+      <pre>{{word_content}}</pre>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 <script>
@@ -27,12 +41,26 @@ export default {
         form: {
           url: '',
         },
-        upload_url:process.env.VUE_APP_API_BASE_URL + ''
+        upload_url: process.env.VUE_APP_API_BASE_URL + '/api/doc/word/word-to-html',
+        dialogVisible: false,
+        word_content: '',
       }
     },
     methods: {
       onHtmlToWordSubmit() {
         htmlToWord(this.form)
+      },
+      handleClose(done) {
+        this.$confirm('确认关闭？')
+          .then(_ => {
+            done();
+          })
+          .catch(_ => {});
+      },
+      uploadSuccess(response, file, fileList) {
+          console.log(response,file,fileList)
+          this.dialogVisible = true
+          this.word_content = response
       }
     }
 }
