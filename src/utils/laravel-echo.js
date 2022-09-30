b70d91c2dc723214c41a04cfe17033cdb34dcbb9
yangliuan <yangliuancn@foxmail.com>
2022-09-30 20:31:07 +0800

    md

--- src/utils/laravel-echo.js
+++ src/utils/laravel-echo.js
@@ -1,15 +1,16 @@
 import Echo from 'laravel-echo';
 
 window.pusher = require('pusher-js');
-
+//REF https://learnku.com/laravel/t/17293
 const laraecho = new Echo({
     broadcaster: 'pusher',
     key: process.env.VUE_APP_LARAECHO_KEY,
-    // authEndpoint: process.env.VUE_APP_API_BASE_URL + '/broadcasting/auth',
+    authEndpoint: process.env.VUE_APP_API_BASE_URL + '/api/broadcasting/auth',
     wsHost: process.env.VUE_APP_LARAECHO_WS_BASE_URL,
     wsPort: process.env.VUE_APP_LARAECHO_WS_PORT,
     forceTLS: false,
     enabledTransports: ['ws'],
+    auth: { headers: { 'Authorization': 'Bearer ' +  '1|3Px8ayhtoTEsw8tQ48vtViCzDsvpzs501FYrR1QD','Accept': 'application/json' } }
 })
 
 export { laraecho }
--- src/views/exhibition.vue
+++ src/views/exhibition.vue
@@ -8,10 +8,28 @@
       <swiper-slide> <img src="@/assets/img/4.gif"> </swiper-slide>
       <swiper-slide> <img src="@/assets/img/5.jpg"> </swiper-slide>
     </swiper>
+    <el-container>
+      <el-header>公共广播</el-header>
+      <el-main>
+        <el-row :gutter="20">
+          <el-col :span="6">
+            <div class="grid-content bg-purple"> 
+              <el-button type="primary" round @click="sendInstruction('prev')">prev</el-button>
+            </div>
+          </el-col>
+          <el-col :span="6">
+            <div class="grid-content bg-purple">
+              <el-button type="primary" round @click="sendInstruction('next')">next</el-button>
+            </div>
+          </el-col>
+        </el-row>
+      </el-main>
+    </el-container>
   </div>
 </template>
 
 <script>
+import { allDevices } from '@/api/control'
 import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
 import { laraecho } from '@/utils/laravel-echo'
 import 'swiper/swiper-bundle.css'
@@ -40,10 +58,11 @@ export default {
       .listen('AllDeviceEvent',(data) => {     
         this.swiperHandleInstruction(data)
       })
-    // laraecho.private('device.1')
-    //   .listen('AuthDeviceEvent',(data) => {     
-    //     this.swiperHandleInstruction(data)
-    //   })
+    laraecho.private('device.1')
+      .listen('AuthDeviceEvent',(data) => {     
+        console.log(data)
+        this.swiperHandleInstruction(data)
+      })
   },
   methods: {
     swiperHandleInstruction(instruction) {
@@ -57,16 +76,88 @@ export default {
             break;
         }
       }
+    },
+    sendInstruction(direction){
+      allDevices({action_type:'move',action:direction})
+      .then((response)=>{
+        console.log(response)
+      })
     }
   }
 }
 </script>
 
 <style lang="scss">
+  .el-header, .el-footer {
+    background-color: #409EFF;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+  
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
 
-#swiper {
-  width:800px;
-  height:600px;
-}
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+  
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+  
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+  
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
+
+  .el-row {
+    margin-bottom: 20px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+
+  .el-col {
+    border-radius: 4px;
+  }
+
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+
+  .bg-purple {
+    background: #d3dce6;
+  }
+
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
+
+  #swiper {
+    width:800px;
+    height:600px;
+  }
 
 </style>
